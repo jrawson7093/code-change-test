a7dfddc44c6aa522e72bbcc31b13878b036c13fb
jrawson7093 <jrawson7093@soham-college.org.uk>
2014-05-08 11:54:08 +0100

    Update greenBottles.py
    Added comments

--- greenBottles.py
+++ greenBottles.py
@@ -1,8 +1,8 @@
 import time
-for i in range(10, 0, -1):
-    for j in range(2):
+for i in range(10, 0, -1): #Count down from 10 to 1
+    for j in range(2): #Do this twice
         print("{0} green bottle(s), hanging on the wall".format(i))
 
     print("And if 1 green bottle should accidently fall,")
     print("They'll be {0} green bottle(s) hanging on the wall.\n".format(i-1))
-    time.sleep(1)
+    time.sleep(1) #Wait for a second
